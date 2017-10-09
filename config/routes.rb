df305d8aec56f67e1730d43911fe9c5bafc9914e
Marcin <marcin.kopanski@gmail.com>
2017-10-09 15:11:14 +0000

    Routes for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'weight', to: 'pages#weight_program'
+  get 'recipes', to: 'pages#recipes'
 end
