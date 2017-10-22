d2a2ab9a35dfcbb529f8e55634d71c445d23c1a5
Marcin Kopański <marcin.kopanski@gmail.com>
2017-10-22 18:43:06 +0200

    Cinfiguration for heroku

--- config/application.rb
+++ config/application.rb
@@ -11,16 +11,18 @@ module DietCoach
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-    
+
     # Strong Params
     # conflig.action_controller.permit_all_parameters = true
-    
+
     # mk custom
     config.eager_load_paths << "#{Rails.root}/lib"
   end
