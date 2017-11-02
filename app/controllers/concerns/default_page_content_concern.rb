c341ec16e8289fad0fad879b843ad70079fa940c
Marcin Kopanski <mk@pwrs.pl>
2017-11-02 16:38:00 +0100

    Blog form fixed, blogs pagination centered

--- app/controllers/concerns/default_page_content_concern.rb
+++ app/controllers/concerns/default_page_content_concern.rb
@@ -1,12 +1,12 @@
 module DefaultPageContentConcern
    extend ActiveSupport::Concern
-  
+
    included do
       before_action :set_page_defaults
    end
-   
+
    def set_page_defaults
-      @page_title = "DevcampPortfolio | My Portfolio Website"
-      @seo_keywords = "Marcin Kopanski portfolio"
+      @page_title = "Gabinet Równowaga"
+      @seo_keywords = ""
    end
 end
