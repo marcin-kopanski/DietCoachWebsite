f47261bf530a370d8fe27fd1013d42e7daaf5ae4
Mikolaj <mikolaj.klein@gmail.com>
2018-05-23 18:17:27 +0000

    config.middleware.use Rack::Deflater

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module DietCoach
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.middleware.use Rack::Deflater
+    
     config.secret_key_base = ENV["SECRET_KEY_BASE"]
     
     config.assets.serve_static_files = true
