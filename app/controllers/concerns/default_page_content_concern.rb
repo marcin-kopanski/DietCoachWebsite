4ae2d069d2b89661ea7279eddb2ea35dcd2eaf80
Mikolaj <mikolaj.klein@gmail.com>
2018-05-23 19:34:19 +0000

    canonical-rails

--- app/controllers/concerns/default_page_content_concern.rb
+++ app/controllers/concerns/default_page_content_concern.rb
@@ -6,8 +6,8 @@ module DefaultPageContentConcern
    end
 
    def set_page_defaults
-      @page_title = "Dietetyk Psycholog Gdańsk"
+      @page_title = "Gabinet terapii psychologicznej i psychodietetycznej Równowaga"
       @page_description = "Poradnia dietetyczna w Gdańsku, układanie diet, doradztwo żywieniowe, motywacja"
-      @seo_keywords = "dietetyk, dietetyk gdańsk, dietetyk gdańsk wrzeszcz, dieta, odchudzanie, równowaga"
+      @seo_keywords = "dietetyk, dietetyk gdańsk, dietetyk gdańsk wrzeszcz, dieta, odchudzanie, równowaga, Patrycja Piekarczyk"
    end
 end
