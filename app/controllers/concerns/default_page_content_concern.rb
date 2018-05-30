b12f037abd604e83332fcd497409b4488faac513
Mikolaj <mikolaj.klein@gmail.com>
2018-05-30 22:05:09 +0000

    zmiana bmi.png, sitemap

--- app/controllers/concerns/default_page_content_concern.rb
+++ app/controllers/concerns/default_page_content_concern.rb
@@ -6,7 +6,7 @@ module DefaultPageContentConcern
    end
 
    def set_page_defaults
-      @page_title = "Gabinet terapii psychologicznej i psychodietetycznej Równowaga"
+      @page_title = "Gabinet Równowaga - Dietetyk Gdańsk Wrzeszcz"
       @page_description = "Poradnia dietetyczna w Gdańsku, układanie diet, doradztwo żywieniowe, motywacja"
       @seo_keywords = "dietetyk, dietetyk gdańsk, dietetyk gdańsk wrzeszcz, dieta, odchudzanie, równowaga, Patrycja Piekarczyk"
    end
