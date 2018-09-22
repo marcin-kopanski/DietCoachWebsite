1fc32679aa6b16303c773750b54f1211af348a71
Mikolaj <mikolaj.klein@gmail.com>
2018-09-22 05:37:00 +0000

    seo, poprawa tytulu, opisu, podzial strony na artykuly

--- app/controllers/concerns/default_page_content_concern.rb
+++ app/controllers/concerns/default_page_content_concern.rb
@@ -6,8 +6,8 @@ module DefaultPageContentConcern
    end
 
    def set_page_defaults
-      @page_title = "Gabinet Równowaga - Dietetyk Gdańsk Wrzeszcz"
-      @page_description = "Poradnia dietetyczna w Gdańsku, układanie diet, doradztwo żywieniowe, motywacja"
+      @page_title = "Dietetyk Gdańsk Wrzeszcz - Gabinet Równowaga"
+      @page_description = "Potrzebujesz indywidualnej diety? Mieszkasz w Gdańsku? dietetyk-rownowaga.gda.pl jest tym czego szukasz! Zdrowe odżywianie potrafi być tanie!"
       @seo_keywords = "dietetyk, dietetyk gdańsk, dietetyk gdańsk wrzeszcz, dieta, odchudzanie, równowaga, Patrycja Piekarczyk"
    end
 end
