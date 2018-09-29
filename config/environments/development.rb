db1213678493de0d74e4b2cbf9a2f36009c51734
Mikolaj <mikolaj.klein@gmail.com>
2018-09-29 17:55:28 +0000

    seo usuniecie niepotrzebnych css'ow bootstrapowych

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,6 +25,9 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+  
+  config.assets.js_compressor = :uglifier
+  
 
   # Don't care if the mailer can't send.
   # mk change to true
@@ -41,7 +44,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
